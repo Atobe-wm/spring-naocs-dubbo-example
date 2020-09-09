fea50187d6cf46bd25df93720e32f1e1228bc5ba
weiwm <weimin@yuantutech.com>
2020-09-09 15:11:49 +0800

    提交代码

--- src/main/java/com/spring/config/NacosConfiguration.java
+++ src/main/java/com/spring/config/NacosConfiguration.java
@@ -3,6 +3,7 @@ package com.spring.config;
 import com.alibaba.nacos.api.annotation.NacosProperties;
 import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -14,9 +15,9 @@ import org.springframework.context.annotation.PropertySource;
  */
 @Configuration
 @PropertySource(value = { "classpath:config.properties" })
+@EnableNacosDiscovery(globalProperties = @NacosProperties(serverAddr = "${nacos.registry.address}",namespace = "${nacos.registry.namespace}"))
 @EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "${nacos.registry.address}",namespace = "${nacos.registry.namespace}"))
 @NacosPropertySource(dataId = "app_config_init.properties", autoRefreshed = true)
-@NacosPropertySource(dataId = "ytmonitor_config.properties", autoRefreshed = true)
 @Slf4j
 public class NacosConfiguration {
 
